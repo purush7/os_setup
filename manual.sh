7ab6cca91115d05dad43faaea2ddea4eaf9b039f
purush7 <linuxlover@gmail.com>
2021-09-12 16:26:44 +0530

    add spotify blocker

--- manual.sh
+++ manual.sh
@@ -11,3 +11,16 @@ cd /usr/share/applications
 sudo echo "[Desktop Entry]\nName=MYKI\nVersion=1.0\nExec=/home/purush/Downloads/MYKI-latest.AppImage" >
 myki.desktop
 sudo update-desktop-database
+
+## install spotify-adblocker
+sudo apt install cargo # dep for adblocker
+## install spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update && sudo apt install spotify-client spotify-client-gnome-support
+
+echo "git clone the spotify-adblocker repo"
+echo "[Desktop Entry]\nType=Application\nName=Spotify (adblock)\nGenericName=Music Player\nIcon=spotify-client\nTryExec=spotify\nExec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U\nTerminal=false\nMimeType=x-scheme-handler/spotify;\nCategories=Audio;Music;Player;AudioVideo;\nStartupWMClass=spotify\n
+" > /usr/share/applications/spotify-adblocker.desktop
+
+sudo update-desktop-database
