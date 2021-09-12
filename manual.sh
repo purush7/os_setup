033b2ac49c828e6cd3a3db8094491a6c58e4b555
purush7 <linuxlover@gmail.com>
2021-09-12 16:49:29 +0530

    block ads in spotify

--- manual.sh
+++ manual.sh
@@ -13,14 +13,25 @@ myki.desktop
 sudo update-desktop-database
 
 ## install spotify-adblocker
-sudo apt install cargo # dep for adblocker
+# sudo apt install cargo # dep for adblocker
 ## install spotify
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install spotify-client spotify-client-gnome-support
 
-echo "git clone the spotify-adblocker repo"
-echo "[Desktop Entry]\nType=Application\nName=Spotify (adblock)\nGenericName=Music Player\nIcon=spotify-client\nTryExec=spotify\nExec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U\nTerminal=false\nMimeType=x-scheme-handler/spotify;\nCategories=Audio;Music;Player;AudioVideo;\nStartupWMClass=spotify\n
-" > /usr/share/applications/spotify-adblocker.desktop
+# echo "git clone the spotify-adblocker repo"
+# echo "[Desktop Entry]\nType=Application\nName=Spotify (adblock)\nGenericName=Music Player\nIcon=spotify-client\nTryExec=spotify\nExec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U\nTerminal=false\nMimeType=x-scheme-handler/spotify;\nCategories=Audio;Music;Player;AudioVideo;\nStartupWMClass=spotify\n" > /usr/share/applications/spotify-adblocker.desktop
 
-sudo update-desktop-database
+# sudo update-desktop-database
+
+# or
+
+echo "##Spotify Blocked ads\n0.0.0.0 pubads.g.doubleclick.net\n0.0.0.0
+securepubads.g.doubleclick.net\n0.0.0.0 http://www.googletagservices.com\n0.0.0.0
+gads.pubmatic.com\n0.0.0.0 ads.pubmatic.com\n0.0.0.0 spclient.wg.spotify.com" >> /etc/hosts
+
+
+## bluetooth
+sudo apt install bluemanager
+
+
