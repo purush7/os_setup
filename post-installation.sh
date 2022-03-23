a4a31736f1cd278cfdf1e97afbab42b1df748e90
purush7 <linuxlover@gmail.com>
2022-03-23 13:00:22 +0530

    add suspend while laptoplid is closed code

--- post-installation.sh
+++ post-installation.sh
@@ -8,6 +8,8 @@ apt install build-essential curl wget git neofetch zsh tree zsh-autosuggestions 
 apt install npm 
 
 # brave
+apt install apt-transport-https curl
+
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
@@ -49,3 +51,27 @@ apt install hugo
 
 ## install ncal or cal
 apt install ncal
+
+## add net tools
+apt install net-tools
+
+## add tlp for extending battery
+apt install tlp
+
+# suspend
+apt install pm-utils
+
+
+
+
+
+
+
+
+
+
+
+
+
+## edit config files for suspend
+echo "HandleSuspendKey=suspend\nHandleLidSwitch=suspend\nHandleLidSwitchDocked=suspend\nHandleHibernateKey=suspend" >> /etc/systemd/logind.conf
