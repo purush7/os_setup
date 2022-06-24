b19312468167c0d5e1efc6f7272cb54753a960ad
purush7 <linuxlover@gmail.com>
2022-06-24 21:48:37 +0530

    add autoconf

--- post-installation.sh
+++ post-installation.sh
@@ -8,8 +8,6 @@ apt install build-essential curl wget git neofetch zsh tree zsh-autosuggestions 
 apt install npm 
 
 # brave
-apt install apt-transport-https curl
-
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
@@ -58,20 +56,5 @@ apt install net-tools
 ## add tlp for extending battery
 apt install tlp
 
-# suspend
-apt install pm-utils
-
-
-
-
-
-
-
-
-
-
-
-
-
-## edit config files for suspend
-echo "HandleSuspendKey=suspend\nHandleLidSwitch=suspend\nHandleLidSwitchDocked=suspend\nHandleHibernateKey=suspend" >> /etc/systemd/logind.conf
+## add autoconf
+apt install autoconf
